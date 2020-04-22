42b6357d6482253d90e151cb809b0bafbd1cdfb2
bayun <i.korniyenko@list.ru>
2020-04-22 21:17:11 +0300

    append some piece of models

--- simple_site/notes/models.py
+++ simple_site/notes/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
 # Create your models here.
+
+class Tag():
+    name_tag = models.CharField(max_length=30)
+
+class Category():
+    name_category = models.CharField(max_length=10)
+
+
+class Note():
+    title = models.CharField(max_length=60)
+    body = models.TextField()
+    category = models.ForeignKey(Category, null=True, on_delete=models.SET_NULL)
+    tags = models.ManyToManyField(Tag)
