70e0a6727e5062a2b7a0303eeb8818436d19ea22
bayun <i.korniyenko@list.ru>
2020-04-22 21:03:41 +0300

    fixed settings

--- simple_site/simple_site/settings.py
+++ simple_site/simple_site/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&zh0d88rs_4)_#onozi_&hh&n&5k=uw!0xj5i&(lwz$^(wu(!5'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
