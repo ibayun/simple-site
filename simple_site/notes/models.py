f13b40cb89e2b3179530170987e49c7fb5dff8e3
bayun <i.korniyenko@list.ru>
2020-04-23 23:23:30 +0300

    some fixed models.  append template, .html,

--- simple_site/notes/models.py
+++ simple_site/notes/models.py
@@ -1,15 +1,20 @@
 from django.db import models
 # Create your models here.
 
-class Tag():
+
+class Tag(models.Model):
     name_tag = models.CharField(max_length=30)
 
-class Category():
+
+class Category(models.Model):
     name_category = models.CharField(max_length=10)
 
 
-class Note():
+class Note(models.Model):
     title = models.CharField(max_length=60)
     body = models.TextField()
-    category = models.ForeignKey(Category, null=True, on_delete=models.SET_NULL)
-    tags = models.ManyToManyField(Tag)
+    category = models.ForeignKey('Category', null=True, on_delete=models.SET_NULL)
+    tags = models.ManyToManyField('Tag')
+
+    # def __str__(self):
+    #     return self.title
--- simple_site/notes/views.py
+++ simple_site/notes/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views import View
+
+from .models  import Note
+
+
+def all_notes(request):
+    notes = Note.objects.all()
+    return render(request, 'notes_t/based_page.html', context={'notes': notes})
