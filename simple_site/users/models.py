21968555474cd64dcf34e1c5ed1f9952cdacd4e6
bayun <i.korniyenko@list.ru>
2020-04-24 21:48:21 +0300

    append models

--- simple_site/users/models.py
+++ simple_site/users/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+    login_user = models.CharField(max_length=30)
+    first_name_user = models.CharField(max_length=30)
+    last_name_user = models.CharField(max_length=30)
+    email_user = models.EmailField(max_length=100)
