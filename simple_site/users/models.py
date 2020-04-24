07bbf443a9950c9cc3296eb7fd9b68fdf3449b22
bayun <i.korniyenko@list.ru>
2020-04-24 23:05:13 +0300

    fields of users were fixed

--- simple_site/users/models.py
+++ simple_site/users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 
 
@@ -8,3 +9,6 @@ class User(models.Model):
     first_name_user = models.CharField(max_length=30)
     last_name_user = models.CharField(max_length=30)
     email_user = models.EmailField(max_length=100)
+    data_of_creation = models.DateField(auto_now_add=True)
+    last_user_visit = models.DateField(auto_now=True)
+
