47f7f0070bb9b4e963c3c78bb61c9b3bbe3469a7
bayun <i.korniyenko@list.ru>
2020-04-26 22:13:42 +0300

    fixed models - note, user

--- simple_site/notes/models.py
+++ simple_site/notes/models.py
@@ -6,9 +6,15 @@ from users.models import User
 class Tag(models.Model):
     name_tag = models.CharField(max_length=30)
 
+    def __str__(self):
+        return self.name_tag
+
 
 class Category(models.Model):
     name_category = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name_category
 
 
 class Note(models.Model):
@@ -18,5 +24,8 @@ class Note(models.Model):
     tags = models.ManyToManyField('Tag')
     created_by = models.ForeignKey(User, null=True, verbose_name='user', on_delete=models.CASCADE)
     data_of_create = models.DateField(auto_now_add=True, null=True)
-    # def __str__(self):
-    #     return self.title
+
+    def __str__(self):
+        return self.title
+
+
--- simple_site/users/models.py
+++ simple_site/users/models.py
@@ -1,14 +1,7 @@
-from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.core.signing import TimestampSigner
 
 
-# Create your models here.
-
-
-class User(models.Model):
-    login_user = models.CharField(max_length=30)
-    first_name_user = models.CharField(max_length=30)
-    last_name_user = models.CharField(max_length=30)
-    email_user = models.EmailField(max_length=100)
-    data_of_register = models.DateField(auto_now_add=True)
-    last_user_visit = models.DateField(auto_now=True)
+class User(AbstractUser):
+    signer = TimestampSigner(salt='Hello')
 
